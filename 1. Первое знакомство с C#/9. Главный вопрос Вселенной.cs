53f26c66013df9c1b252bb8b520035a9c4e13734
Artyom Shutoff <37169190+artyomshutoff@users.noreply.github.com>
2022-09-30 19:33:52 +0300

    Add files via upload

--- 1. Первое знакомство с C#/9. Главный вопрос Вселенной.cs
+++ 1. Первое знакомство с C#/9. Главный вопрос Вселенной.cs
@@ -1,9 +1,5 @@
-static int GetSquare(double a)
-		{
-			// return указывает, какое значение будет возвращено
-			return (int)Math.Pow(a,2);
-		}
-static void Print(int number)
-		{
-			Console.WriteLine(number);
-		}
+static int GetSquare(double a) {
+    // return указывает, какое значение будет возвращено
+    return (int) Math.Pow(a, 2);
+}
+static void Print(int number) { Console.WriteLine(number); }
