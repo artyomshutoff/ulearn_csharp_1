178369ef5a0df8f2410ed3ef6093636756b994b0
Artyom Shutoff <37169190+artyomshutoff@users.noreply.github.com>
2022-10-01 10:15:11 +0300

    Add files via upload

---
+++ 14. Целостность данных/4. Вектор.cs
@@ -0,0 +1,25 @@
+public class Vector
+{
+    public double X
+    {
+        get;
+        set;
+    }
+    public double Y
+    {
+        get;
+        set;
+    }
+    public double Length => Math.Sqrt(X * X + Y * Y);
+
+    public Vector(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("({0}, {1}) with length: {2}", X, Y, Length);
+    }
+}
