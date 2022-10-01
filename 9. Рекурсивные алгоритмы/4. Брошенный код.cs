c553797316cda72f56a71a97cf3f41d351cea96f
Artyom Shutoff <37169190+artyomshutoff@users.noreply.github.com>
2022-10-01 10:00:10 +0300

    Add files via upload

---
+++ 9. Рекурсивные алгоритмы/4. Брошенный код.cs
@@ -0,0 +1,16 @@
+static void
+MakePermutations(int[] permutation, int position, List<int[]> result)
+{
+    if (position == permutation.Length) {
+        WritePermutation(permutation);
+        return;
+    } else {
+        for (int i = 0; i < permutation.Length; i++) {
+            var index = Array.IndexOf(permutation, i, 0, position);
+            if (index == -1) {
+                permutation[position] = i;
+                MakePermutations(permutation, position + 1, result);
+            }
+        }
+    }
+}
